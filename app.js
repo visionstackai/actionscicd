35e54de48e4898ddeac5b2d519772a77ba86c564
janaavula <jana.avula@gmail.com>
2023-04-17 13:50:46 -0400

    added rate limiting

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const bodyParser = require('body-parser')
 const app = express()
 const skuRouter = require('./src/routes/sku')
 const bookRouter = require('./src/routes/book')
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
 
 //  the logMiddleware function is added to the Express application using the app.use() method. This function logs the request method and URL for every incoming request, and then calls the next() function to pass control to the next middleware function in the stack. If there are no more middleware functions, the final request handler for the route is executed.
 const logMiddleware = (req, res, next) => {
@@ -17,6 +24,10 @@ app.use(
     extended: true
   })
 )
+
+app.use("/api/resources", limiter)
+app.use('/test', limiter)
+
 app.use('/api/resources', skuRouter)
 app.use('/test', bookRouter)
 
